22d92416b3419d7e6e587a363adb5a452cc609f6
Abdulaziz <ahrhq2004@gmail.com>
2025-04-14 10:34:21 +0300

    Working on Task Page Almost 80%

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -33,10 +33,3 @@ function checkIsUpcoming(date) {
 
 // ----------------------- Main Function -----------------------
 
-export function Tasks() {
-  return (
-    <div>
-        
-    </div>
-  );
-}
