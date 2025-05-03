b09be9fd1b9b5f19e82450d38b47937eb849d1bf
Mohannad <almohnd2014@gmail.com>
2025-05-03 21:30:07 +0300

    edit on integrationSchema

--- server/models/integrationSchema.js
+++ server/models/integrationSchema.js
@@ -3,11 +3,16 @@ const { Schema } = mongoose;
 
 const integrationSchema = new Schema(
   {
+    // Calendar Integrations
     googleCalendar: { type: Boolean, default: false },
-    slack:          { type: Boolean, default: false },
-    notion:         { type: Boolean, default: false },
+    notion: { type: Boolean, default: false },
+    todoist: { type: Boolean, default: false },
+    
+    // Collaboration Tools
+    slack: { type: Boolean, default: false },
+    github: { type: Boolean, default: false },
   },
   { _id: false }
 );
 
-module.exports = integrationSchema; 
+module.exports = integrationSchema;
